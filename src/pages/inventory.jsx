f6aa21a2fde5d3278746c018bf031970c17bd332
BenEspinoza10 <bj.nico10.000@gmail.com>
2022-11-21 23:40:27 -0300

    Routing
    Se hizo routing de las 3 paginas principales que poseerá la app

---
+++ src/pages/inventory.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import NavBar from '../components/nav_bar'
+
+export const Inventory = () => {
+  return (
+    <div className='page'>
+      <h1 className='page__title'>Frontend Sample App</h1>
+      <NavBar />
+      <h2>Este es un inventario</h2>
+      
+      <p>j</p>
+    </div>
+  )
+}
+
+export default Inventory
